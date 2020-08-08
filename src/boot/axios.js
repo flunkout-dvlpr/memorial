a7c5cdef3f453b7b23a639d0483f7f6a60bf0094
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-08 15:50:26 -0500

    Set base url for axios

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+if (process.env.PROD) {
+  axios.defaults.baseURL = 'https://uh00rtja7j.execute-api.us-east-2.amazonaws.com/Prod'
+} else {
+  axios.defaults.baseURL = 'http://localhost:3000/'
+}
+
 Vue.prototype.$axios = axios
