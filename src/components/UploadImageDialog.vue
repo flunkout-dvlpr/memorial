d60e1e74b57aa3295efa2a97d1aaf33953716d73
flunkout-dvlpr <info@juliogonzalez.space>
2020-08-09 00:01:23 -0500

    Create component to handle image upload using Uploader

---
+++ src/components/UploadImageDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-dialog ref="dialog" @hide="onDialogHide">
+    <q-uploader
+      :url="url"
+      method="PUT"
+      :headers="[{'Content-Type': 'image/png'}]"
+      max-files="1"
+      color="primary"
+      :send-raw="true"
+      @finish="hide"
+    />
+  </q-dialog>
+</template>
+
+<script>
+export default {
+  name: 'UploadImageDialog',
+  props: {
+    url: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    show () {
+      this.$refs.dialog.show()
+    },
+    hide () {
+      this.$refs.dialog.hide()
+    },
+    onDialogHide () {
+      this.$emit('hide')
+    }
+  }
+}
+</script>
